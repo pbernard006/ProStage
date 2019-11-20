d426392cb4d08d757f0997e88c05f8a67b072240
Paul BERNARD <pbernard006@iutbayonne.univ-pau.fr>
2019-11-20 14:41:34 +0100

    Externalisation des routes dans le fichier routes.yaml

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -7,9 +7,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ProStageController extends AbstractController
 {
-    /**
-     * @Route("/pro/stage", name="pro_stage")
-     */
+   
     public function index()
     {
         return $this->render('pro_stage/index.html.twig', [

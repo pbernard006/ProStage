069080f65ba01cbe532ebf9ad34b3b3e2d23959a
Paul BERNARD <pbernard006@iutbayonne.univ-pau.fr>
2019-11-20 19:33:55 +0100

    Création page dynamique des stages

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -14,4 +14,24 @@ class ProStageController extends AbstractController
             'controller_name' => 'ProStageController',
         ]);
     }
+
+    public function afficherEntreprises()
+    {
+        return $this->render('pro_stage/afficherEntreprises.html.twig', [
+            'controller_name' => 'ProStageController',
+        ]);
+    }
+
+    public function afficherFormations()
+    {
+        return $this->render('pro_stage/afficherFormations.html.twig', [
+            'controller_name' => 'ProStageController',
+        ]);
+    }
+
+    public function afficherStages($id)
+    {
+        return $this->render('pro_stage/afficherStages.html.twig',['id' => $id ]);
+    }
 }
+
